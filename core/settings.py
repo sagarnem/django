67fdd95acaccdcdcfc8e49222f4a8b21663e3f5b
Sagarnem <sagarnemkul7@gmail.com>
2024-06-27 19:28:48 +0545

    media

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "public/media/")
+
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
